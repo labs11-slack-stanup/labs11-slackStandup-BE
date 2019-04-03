7250a2f407cba31ad9102442b144e1980e638a32
Samsoucy <newf930@outlook.com>
2019-04-03 16:12:03 -0400

    removed three POST endpoints from stripe router. Now have one POST for the paid subscription to curie

--- back-end/api/routes/stripeRouter.js
+++ back-end/api/routes/stripeRouter.js
@@ -15,41 +15,8 @@ const {
     serverErrorDelete500,
     serverErrorUpdate404,
     serverErrorUpdate500
-  } = require("./routeHelpers/helpers.js");
-
-
-  router.post('/subscribe50', (req, res) => {
-    const stripeToken = req.body.stripeToken;
-    const email = req.body.email;
-
-    stripe.customers.create({
-        email: email,
-        source: stripeToken
-    }, function(err, customer) {
-        if(err) {
-            res.send(
-                serverErrorPost(res)
-            )
-        } else {
-            const { id } = customer
-
-            stripe.subscriptions.create({
-                customer: id,
-                items: [{plan: 'plan_EoTZWg3g8rXaYL'}],
-
-            }, function(err, subscription) {
-                if(err) {
-                    res.send(
-                        serverErrorPost(res)
-                    )
-                } else {
-                    res.status(200).json({subscription})
-                }
-            })
-        }
-    })
-})
-
+} = require("./routeHelpers/helpers.js");
+  
 router.post('/subscribe10', (req, res) => {
     const stripeToken = req.body.stripeToken;
     const email = req.body.email;
@@ -67,7 +34,7 @@ router.post('/subscribe10', (req, res) => {
 
             stripe.subscriptions.create({
                 customer: id,
-                items: [{plan: 'plan_EoTXFXkRKtTHBa'}],
+                items: [{plan: 'plan_EoT9REg5JfI6KN'}],
 
             }, function(err, subscription) {
                 if(err) {
@@ -84,70 +51,105 @@ router.post('/subscribe10', (req, res) => {
     })
 })
 
-router.post('/subscribe75', (req, res) => {
-    const stripeToken = req.body.stripeToken;
-    const email = req.body.email;
 
-    stripe.customers.create({
-        email: email,
-        source: stripeToken
-    }, function(err, customer) {
-        if(err) {
-            res.send(
-                serverErrorPost(res)
-            )
-        } else {
-            const { id } = customer
+//   router.post('/subscribe50', (req, res) => {
+//     const stripeToken = req.body.stripeToken;
+//     const email = req.body.email;
 
-            stripe.subscriptions.create({
+//     stripe.customers.create({
+//         email: email,
+//         source: stripeToken
+//     }, function(err, customer) {
+//         if(err) {
+//             res.send(
+//                 serverErrorPost(res)
+//             )
+//         } else {
+//             const { id } = customer
+
+//             stripe.subscriptions.create({
+//                 customer: id,
+//                 items: [{plan: 'plan_EoTZWg3g8rXaYL'}],
+
+//             }, function(err, subscription) {
+//                 if(err) {
+//                     res.send(
+//                         serverErrorPost(res)
+//                     )
+//                 } else {
+//                     res.status(200).json({subscription})
+//                 }
+//             })
+//         }
+//     })
+// })
+
+
+
+// router.post('/subscribe75', (req, res) => {
+//     const stripeToken = req.body.stripeToken;
+//     const email = req.body.email;
+
+//     stripe.customers.create({
+//         email: email,
+//         source: stripeToken
+//     }, function(err, customer) {
+//         if(err) {
+//             res.send(
+//                 serverErrorPost(res)
+//             )
+//         } else {
+//             const { id } = customer
+
+//             stripe.subscriptions.create({
                 
-                customer: id,
-                items: [{plan: 'plan_EoT9REg5JfI6KN'}],
+//                 customer: id,
+//                 items: [{plan: 'plan_EoT9REg5JfI6KN'}],
 
-            }, function(err, subscription) {
-                if(err) {
-                    res.send(
-                        serverErrorPost(res)
-                    )
-                } else {
-                    res.status(200).json({subscription})
-                }
-            })
-        }
-    })
-})
+//             }, function(err, subscription) {
+//                 if(err) {
+//                     res.send(
+//                         serverErrorPost(res)
+//                     )
+//                 } else {
+//                     res.status(200).json({subscription})
+//                 }
+//             })
+//         }
+//     })
+// })
 
-router.post('/subscribe15', (req, res) => {
-    const stripeToken = req.body.stripeToken;
-    const email = req.body.email;
+// router.post('/subscribe15', (req, res) => {
+//     const stripeToken = req.body.stripeToken;
+//     const email = req.body.email;
 
-    stripe.customers.create({
-        email: email,
-        source: stripeToken
-    }, function(err, customer) {
-        if(err) {
-            res.send(
-                serverErrorPost(res)
-            )
-        } else {
-            const { id } = customer
+//     stripe.customers.create({
+//         email: email,
+//         source: stripeToken
+//     }, function(err, customer) {
+//         if(err) {
+//             res.send(
+//                 serverErrorPost(res)
+//             )
+//         } else {
+//             const { id } = customer
 
-            stripe.subscriptions.create({
+//             stripe.subscriptions.create({
 
-                customer: id,
-                items: [{plan: 'plan_EoT9diKu1uLPVM'}],
+//                 customer: id,
+//                 items: [{plan: 'plan_EoT9diKu1uLPVM'}],
 
-            }, function(err, subscription) {
-                if(err) {
-                    res.send(
-                        serverErrorPost(res)
-                    )
-                } else {
-                    res.status(200).json({subscription})
-                }
-            })
-        }
-    })
-})
+//             }, function(err, subscription) {
+//                 if(err) {
+//                     res.send(
+//                         serverErrorPost(res)
+//                     )
+//                 } else {
+//                     res.status(200).json({subscription})
+//                 }
+//             })
+//         }
+//     })
+// })
 
 module.exports = router;
