b31d354da42763f0e17b978f3388e6eacc74f371
J A <jarata@gmail.com>
2019-04-03 13:22:08 -0700

    swapped answers notNullable to questions

--- back-end/api/database/migrations/20190327132327_questions_survey_table.js
+++ back-end/api/database/migrations/20190327132327_questions_survey_table.js
@@ -3,12 +3,12 @@ exports.up = function(knex, Promise) {
     return knex.schema.createTable("questionSurveys", tbl => {
       tbl.increments("id");
       tbl.string("title").notNullable();
-      tbl.string("question_1")
-      tbl.string("answer_1").notNullable();
-      tbl.string("question_2")
-      tbl.string("answer_2").notNullable();
-      tbl.string("question_3")
-      tbl.string("answer_3").notNullable();
+      tbl.string("question_1").notNullable();
+      tbl.string("answer_1")
+      tbl.string("question_2").notNullable();
+      tbl.string("answer_2")
+      tbl.string("question_3").notNullable();
+      tbl.string("answer_3")
       tbl.integer("manager_id").notNullable();
       tbl.timestamp("created_at", true).defaultTo(knex.fn.now())
     });
