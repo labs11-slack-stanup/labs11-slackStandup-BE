e86ef85f896631612bf4f03dd9602c5b9aeb2647
Samsoucy <newf930@outlook.com>
2019-04-03 11:59:07 -0400

    put in pk stripekey for both frontend billing file buttons to link them to stripe account

--- front-end/sentimentbot/src/Components/billing/billing.js
+++ front-end/sentimentbot/src/Components/billing/billing.js
@@ -105,7 +105,7 @@ class Billing extends React.Component {
                 <StripeCheckout
                   className="stripe-checkout"
                   token={this.onToken}
-                  stripeKey="sk_test_UsTtkoRZpYswzuQJ15TTqZeN00xFNybI2L"
+                  stripeKey="pk_test_duHUFLqPSBcHqEFQC0U3RhhZ00yfnpiQNy"
                   image="https://i.gyazo.com/59da540e04d2a13f71991c8b168a4ab5.png"
                   description="$10/mo"
                   name="M.O.O.D."
@@ -126,7 +126,7 @@ class Billing extends React.Component {
                 <StripeCheckout
                   className="stripe-checkout"
                   token={this.onToken2}
-                  stripeKey="sk_test_UsTtkoRZpYswzuQJ15TTqZeN00xFNybI2L"
+                  stripeKey="pk_test_duHUFLqPSBcHqEFQC0U3RhhZ00yfnpiQNy"
                   image="https://i.gyazo.com/59da540e04d2a13f71991c8b168a4ab5.png"
                   description="$50/mo"
                   name="M.O.O.D."
