53754c5b5701ea1c869fa7942222d13caf5d2442
Sheila <s.hopper01110@gmail.com>
2019-04-04 15:44:20 -0400

    removed answers from questions survey table

--- back-end/api/database/migrations/20190327132327_questions_survey_table.js
+++ back-end/api/database/migrations/20190327132327_questions_survey_table.js
@@ -4,11 +4,8 @@ exports.up = function(knex, Promise) {
       tbl.increments("id");
       tbl.string("title").notNullable();
       tbl.string("question_1").notNullable();
-      tbl.string("answer_1")
       tbl.string("question_2").notNullable();
-      tbl.string("answer_2")
       tbl.string("question_3").notNullable();
-      tbl.string("answer_3")
       tbl.integer("manager_id").notNullable();
       tbl.timestamp("created_at", true).defaultTo(knex.fn.now())
       tbl.string("survey_time_stamp");
