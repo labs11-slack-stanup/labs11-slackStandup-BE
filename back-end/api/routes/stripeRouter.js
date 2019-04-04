c0ccfdb74e747d7d028dc621917a234f0af3e8db
Samsoucy <newf930@outlook.com>
2019-04-04 13:50:53 -0400

    add teammember database

--- back-end/api/routes/stripeRouter.js
+++ back-end/api/routes/stripeRouter.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const teamMembersDb = require("../database/helpers/teamMembersDb");
 require('dotenv').config();
 
 const stripe = require('stripe')(process.env.SECRETKEY);
