0986723218912a8c4ed2c21872b799aff3a4c17c
bluelogix <bluelogix010@gmail.com>
2019-04-04 15:37:19 -0400

    fixed active survey from null to show id

---
+++ back-end/api/routes/curieActiveSurvey.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const router = express.Router();
+const db = require("../database/helpers/curieSurveyActiveDb");
+
+const {
+  postSuccess,
+  serverErrorPost,
+  getSuccess,
+  serverErrorGet,
+  serverErrorGetID,
+  serverErrorDelete404,
+  serverErrorDelete500,
+  serverErrorUpdate404,
+  serverErrorUpdate500
+} = require("./routeHelpers/helpers.js");
+
+// const type = "active_surveys";
+
+router.get("/", (req, res) => {
+  db.get()
+    .then(getSuccess(res))
+    .catch(serverErrorGet(res));
+});
+
+module.exports = router;
