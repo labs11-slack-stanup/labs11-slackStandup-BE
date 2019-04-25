7572a25ac46e97e09b5beb73ba2e36650a489661
Samsoucy <newf930@outlook.com>
2019-04-24 21:19:20 -0400

    changed color of nav bar and dropdown, create survey button and containers

--- front-end/sentimentbot/src/Components/NavBar/NavBar.js
+++ front-end/sentimentbot/src/Components/NavBar/NavBar.js
@@ -37,7 +37,7 @@ class NavBar extends React.Component {
   }
   render() {
     const backgroundColor = {
-      backgroundColor: "#02C8A7"
+      backgroundColor: "#efb991"
     };
 
     const fontColor = {
@@ -64,7 +64,7 @@ class NavBar extends React.Component {
                     </NavItem>
                     <NavItem>
                       <NavLink>
-                        <div onClick={() => history.push('/teamlist')}>Teamlist</div>
+                        <div onClick={() => history.push('/teamlist')}>Team List</div>
                       </NavLink>
                     </NavItem>
                     {/* <NavItem>
